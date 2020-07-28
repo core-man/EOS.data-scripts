34dc6a54caa3374b221adb5572ce3b1e31702fc0
core-man <coreman.seism@gmail.com>
2020-07-28 21:51:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+Perl scripts to extract seismic waveforms at EOS
+
+- [catalog](catalog/) : catalog directory
+- [scripts](scripts/) : Perl scripts
+- [station](station/) : station information
+
+
+Go to [scripts](scripts/) to run the scripts
+
---
+++ scripts/extract-tele.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# extract seismic data at EOS, using EOS-data in https://github.com/core-man/seismic-data.
+# copy this script into EOS-data/scripts, and run it
+
+# input
+catalog=/home/tomoboy/Desktop/RF.Myanmar/catalog/events/ComCat-20160101-20200728-tele.dat
+station=../station/MMEOS-coord.dat
+mseedir=/run/media/tomoboy/4T-YAO1/EOS-Myanmar/mseed
+sacdir=/run/media/tomoboy/4T-YAO7/Myanmar/DATA/ENZ-origin
+
+./ExtractWaveform.pl -C$catalog -S$station -M$mseedir -O$sacdir -Eak135 -T-50,150 -BH
+
